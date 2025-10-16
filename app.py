e8b89f0ea4d3e40d06429fcb56be9efa15a2950e
JozeDavhiid <jose05rm@gmail.com>
2025-10-16 09:45:38 -0500

    Actualizacion

--- app.py
+++ app.py
@@ -381,3 +381,5 @@ def acta_pdf(rid):
 # -----------------------
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
+#Proyecto de Mantenimiento
