447a77159ac623410c9ae158f2c6986a796fe51c
JozeDavhiid <jose05rm@gmail.com>
2025-10-21 09:31:47 -0500

    Actualizacion

--- app.py
+++ app.py
@@ -635,7 +635,7 @@ def principal():
                     (sede, fecha, area, tecnico, nombre_maquina, usuario, tipo_equipo, marca, modelo, serial,
                      sistema_operativo, office, antivirus, compresor, control_remoto, activo_fijo, observaciones, ciclo_id, empresa_id)
                      VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''', datos)
-        conn.commit()Bienvenido
+        conn.commit()
         flash('Registro guardado correctamente', 'success')
 
     # Determinar ciclo para consultas
